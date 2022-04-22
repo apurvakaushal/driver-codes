5e0d785959297c193465d621efcc53dcef4719dd
apurvakaushal <104191845+apurvakaushal@users.noreply.github.com>
2022-04-22 12:53:50 +0530

    Update cpp-driver-code.cpp

--- strings/n_inputs/cpp-driver-code.cpp
+++ strings/n_inputs/cpp-driver-code.cpp
@@ -5,6 +5,14 @@ using namespace std;
 
 void solve (int opening_hh, int opening_mm, int duration_hh, int duration_mm) {
     // Your code goes here
+     int start_mins = opening_hh*60 + opening_mm;
+    int duration_mins = duration_hh * 60 + duration_mins;
+    int total_mins = start_mins + duration_mins;
+    int total_hrs = total_mins/60;
+    int rem_mins = total_mins % 60;
+    int closing_hh = total_hrs % 24;
+    int closing_mm = rem_mins;
+    cout << closing_hh << " " << closing_mm << endl;
   
     // Print the output here
 }
